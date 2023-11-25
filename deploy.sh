5f646af74be2a211dabe56f0b3e8ae85afde7961
Asato Wakisaka <asato.wakisaka@gmail.com>
2023-11-25 10:58:02 +0900

    これでどうだ

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 set -xue
 
 cd /home/isucon/webapp/go
-sudo -u isucon git pull origin main
+#sudo -u isucon git pull origin main
 systemctl stop isupipe-go.service
-sudo -u isucon make build
+sudo -u isucon bash -c 'source ~/.profile && make build'
 systemctl restart isupipe-go.service
 # TODO: ここに pdns の再起動とか入れる？
 systemctl reload nginx
