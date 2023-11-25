4e37baa1c4918f207fa50cf7befbe3cb3486dd36
Asato Wakisaka <asato.wakisaka@gmail.com>
2023-11-25 16:17:04 +0900

    Redis に入れる準備

--- go/redis.go
+++ go/redis.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/labstack/echo/v4"
@@ -38,4 +40,18 @@ func getRankOfUser(ctx context.Context, userName string) (int, error) {
 	}
 	// revrank は 0 が最小なので +1 する
 	return int(rank) + 1, err
+}
+
+const userIconHashKeyPrefix = "UserIconHash:"
+
+func userIconHashKey(userName string) string {
+	return fmt.Sprintf("%s:%s", userIconHashKeyPrefix, userName)
+}
+
+func setUserIconSha256(ctx context.Context, userName string, sha256 string) error {
+	return redisClient.Set(ctx, userIconHashKey(userName), sha256, 1*time.Minute).Err()
+}
+
+func getUserIconSha256(ctx context.Context, userName string) (string, error) {
+	return redisClient.Get(ctx, userIconHashKey(userName)).Result()
 }
